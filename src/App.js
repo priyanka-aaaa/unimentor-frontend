eb6bf7cfcf418d8463eb486951e9286b282d794d
Priyanka <priyanka.calinfo500@gmail.com>
2022-03-06 17:48:43 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
 import 'react-phone-number-input/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
-// import './components/css/style.css';
-// import './components/css/dashboard.css';
-// import './components/css/frontend.css';
-
-
 import "./css/drag.css";
-
-
-
 import './css/style.css';
 import './css/dashboard.css';
 import './css/frontend.css';
 import './css/my.css';
 import "./css/drag.css";
 import "./css/responsive.css";
-
-
-// import './components/admin_css/style.css';
 import Studentregister from './components/Studentregister';
 import Studentlogin from './components/Studentlogin';
 import StudentDashboard from './components/StudentDashboard';
@@ -33,12 +20,6 @@ import Document from './components/Student/Document';
 import AddStudent from './components/agent/AddStudent';
 import Bookmark from './components/Student/Bookmark';
 import StudentSetting from './components/Student/StudentSetting';
-
-
-
-
-
-
 import Student from './components/Home/Student';
 
 
@@ -104,130 +85,74 @@ import FinalFilter from './components/Home/FinalFilter';
 import Recruitment from './components/Home/Recruitment';
 import School from './components/Home/School';
 
-
-
-// import Counter from './components/Student/Counter';
 function App() {
   return (
     <div>
-
-      {/* <Counter /> */}
-
-
       <Router>
         <div>
-
-          {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-              <li><Link to={'/'} className="nav-link"> Home </Link></li>
-              <li><Link to={'/Studentlogin'} className="nav-link">Student Login</Link></li>
-              <li><Link to={'/Studentregister'} className="nav-link">Student Register</Link></li>
-              <Link to={'/StudentDashboard'}></Link>
-              <Link to={'/Example'}></Link>
-              <Link to={'/Dashboard'}></Link>
-           
- 
-            </ul>
-          </nav>
-          <hr /> */}
-
-
-
-
-
           <Switch>
-          
-          <Route path='/Student' component={Student} />
-          <Route path='/Recruitment' component={Recruitment} />
-
-          
-            <Route path='/country' component={country} />
-            <Route path='/Degree' component={Degree} />
-            <Route path='/EducationLevel' component={EducationLevel} />
-
-            <Route path='/MajorPursue' component={MajorPursue} />
-
-            <Route path='/Test' component={Test} />
-            <Route path='/AcademicTest' component={AcademicTest} />
-            <Route path='/Experience' component={Experience} />
-            <Route path='/Activity' component={Activity} />
-            <Route path='/NgoExperience' component={NgoExperience} />
-            <Route path='/FinalFilter' component={FinalFilter} />
-            <Route path='/AdminProfile' component={AdminProfile} />
-    
-
-
-            
-
-            <Route path='/AgentRegister' component={AgentRegister} />
-            <Route path='/AgentLogin' component={AgentLogin} />
-            <Route path='/AgentDashboard' component={AgentDashboard} />
-            <Route path='/AgentTeam' component={AgentTeam} />
-            <Route path='/AgentStudent' component={AgentStudent} />
-            <Route path='/AgentEvaluate' component={AgentEvaluate} />
-            <Route path='/AgentCertificate' component={AgentCertificate} />
-            <Route path='/AgentApplication' component={AgentApplication} />
-
-            <Route path='/AgentCommission' component={AgentCommission} />
-
-
-
-            <Route exact path='/' component={Home} />
-
-            <Route  path='/About' component={About} />
-            <Route  path='/Contact' component={Contact} />
-            <Route  path='/School' component={School} />
-   
-            
-
-           
-            <Route path='/Universitylogin' component={Universitylogin} />
-            <Route path='/Universityregister' component={Universityregister} />
-            <Route path='/UniversitySetcommission' component={UniversitySetcommission} />
-            <Route path='/UniversityDashboard' component={UniversityDashboard} />
-            <Route path='/UniversityCourses' component={UniversityCourses} />
-            <Route path='/UniversityPassword' component={UniversityPassword} />
-            <Route path='/UniversityIntake' component={UniversityIntake} />
-            <Route path='/UniversityProfile' component={UniversityProfile} />
-            <Route path='/UniversitySummary' component={Summary} />
-            <Route path='/AllUniversity' component={AllUniversity} />
-
-            <Route path='/institute/:id' component={UniveristyPage} />
-        
-            {/* <Route exact path="/institutemy" render={() => (<UniveristyPage2 id={54} />)} /> */}
-            <Route path='/institutemy' component={UniveristyPage2} />
-            <Route path='/AdminLogin' component={AdminLogin} />
-            <Route path='/AdminDashboard' component={AdminDashboard} />
-            <Route path='/AdminStudent' component={AdminStudent} />
-            <Route path='/AdminAgent' component={AdminAgent} />
-            <Route path='/AdminUniversity' component={AdminUniversity} />
-         
-       
-            <Route path='/AdminDashboard' component={AdminDashboard} />
-            <Route path='/AdminApplication' component={AdminApplication} />
-            <Route path='/AdminDocument' component={AdminDocument} />
-            <Route path='/AdminScholarship' component={AdminScholarship} />
-            <Route path='/AdminStudentApplication' component={AdminStudentApplication} />
-            <Route path='/AdminStudentApplicationView' component={AdminStudentApplicationView} />
-            <Route path='/ApplicationStep' component={ApplicationStep} />
-            <Route path='/ViewStudentApplication' component={ViewStudentApplication} />
-
-            
-            <Route path='/StudentSetting' component={StudentSetting} />
-
-            <Route path='/Studentlogin' component={Studentlogin} />
-            <Route path='/Studentregister' component={Studentregister} />
-            <Route path='/StudentDashboard' component={StudentDashboard} />
-            <Route path='/StudentForgotPassword' component={StudentForgotPassword} />
-            <Route path='/Dashboard' component={Dashboard} />
-            <Route path='/ApplicationProfile' component={ApplicationProfile} />
-            <Route path='/Profile' component={Profile} />
-            <Route path='/Document' component={Document} />
-
-            <Route path='/Bookmark' component={Bookmark} />
-            <Route path='/AddStudent' component={AddStudent} />
-
-
+            <Route path='/Student' component={Student}/>
+            <Route path='/Recruitment' component={Recruitment}/>
+            <Route path='/country' component={country}/>
+            <Route path='/Degree' component={Degree}/>
+            <Route path='/EducationLevel' component={EducationLevel}/>
+            <Route path='/MajorPursue' component={MajorPursue}/>
+            <Route path='/Test' component={Test}/>
+            <Route path='/AcademicTest' component={AcademicTest}/>
+            <Route path='/Experience' component={Experience}/>
+            <Route path='/Activity' component={Activity}/>
+            <Route path='/NgoExperience' component={NgoExperience}/>
+            <Route path='/FinalFilter' component={FinalFilter}/>
+            <Route path='/AdminProfile' component={AdminProfile}/>
+            <Route path='/AgentRegister' component={AgentRegister}/>
+            <Route path='/AgentLogin' component={AgentLogin}/>
+            <Route path='/AgentDashboard' component={AgentDashboard}/>
+            <Route path='/AgentTeam' component={AgentTeam}/>
+            <Route path='/AgentStudent' component={AgentStudent}/>
+            <Route path='/AgentEvaluate' component={AgentEvaluate}/>
+            <Route path='/AgentCertificate' component={AgentCertificate}/>
+            <Route path='/AgentApplication' component={AgentApplication}/>
+            <Route path='/AgentCommission' component={AgentCommission}/>
+            <Route exact path='/' component={Home}/>
+            <Route path='/About' component={About}/>
+            <Route path='/Contact' component={Contact}/>
+            <Route path='/School' component={School}/>
+            <Route path='/Universitylogin' component={Universitylogin}/>
+            <Route path='/Universityregister' component={Universityregister}/>
+            <Route path='/UniversitySetcommission' component={UniversitySetcommission}/>
+            <Route path='/UniversityDashboard' component={UniversityDashboard}/>
+            <Route path='/UniversityCourses' component={UniversityCourses}/>
+            <Route path='/UniversityPassword' component={UniversityPassword}/>
+            <Route path='/UniversityIntake' component={UniversityIntake}/>
+            <Route path='/UniversityProfile' component={UniversityProfile}/>
+            <Route path='/UniversitySummary' component={Summary}/>
+            <Route path='/AllUniversity' component={AllUniversity}/>
+            <Route path='/institute/:id' component={UniveristyPage}/>
+            <Route path='/institutemy' component={UniveristyPage2}/>
+            <Route path='/AdminLogin' component={AdminLogin}/>
+            <Route path='/AdminDashboard' component={AdminDashboard}/>
+            <Route path='/AdminStudent' component={AdminStudent}/>
+            <Route path='/AdminAgent' component={AdminAgent}/>
+            <Route path='/AdminUniversity' component={AdminUniversity}/>
+            <Route path='/AdminDashboard' component={AdminDashboard}/>
+            <Route path='/AdminApplication' component={AdminApplication}/>
+            <Route path='/AdminDocument' component={AdminDocument}/>
+            <Route path='/AdminScholarship' component={AdminScholarship}/>
+            <Route path='/AdminStudentApplication' component={AdminStudentApplication}/>
+            <Route path='/AdminStudentApplicationView' component={AdminStudentApplicationView}/>
+            <Route path='/ApplicationStep' component={ApplicationStep}/>
+            <Route path='/ViewStudentApplication' component={ViewStudentApplication}/>
+            <Route path='/StudentSetting' component={StudentSetting}/>
+            <Route path='/Studentlogin' component={Studentlogin}/>
+            <Route path='/Studentregister' component={Studentregister}/>
+            <Route path='/StudentDashboard' component={StudentDashboard}/>
+            <Route path='/StudentForgotPassword' component={StudentForgotPassword}/>
+            <Route path='/Dashboard' component={Dashboard}/>
+            <Route path='/ApplicationProfile' component={ApplicationProfile}/>
+            <Route path='/Profile' component={Profile}/>
+            <Route path='/Document' component={Document}/>
+            <Route path='/Bookmark' component={Bookmark}/>
+            <Route path='/AddStudent' component={AddStudent}/>
           </Switch>
         </div>
       </Router>
