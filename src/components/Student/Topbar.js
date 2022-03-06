6ab44e3c23365e319ab81da81327df400ca51e10
Priyanka <priyanka.calinfo500@gmail.com>
2022-03-06 14:49:48 +0530

    change in topbar

--- src/components/Student/Topbar.js
+++ src/components/Student/Topbar.js
@@ -291,10 +291,15 @@ class Topbar extends Component {
                       
                         <div className="topbar-divider d-none d-sm-block"></div>
                         <li className="nav-item dropdown no-arrow">
+                            
                             <a className="nav-link dropdown-toggle" href="#collapseEleven" id="userDropdown" role="button" data-bs-toggle="collapse" aria-haspopup="true" aria-expanded="false">
+                            <span className="mr-2 d-none d-lg-inline text-gray-600 small">{this.state.email}</span>
+
                                 <img className="img-profile rounded-circle" src={undraw_profile}
                                 />
+                                
                             </a>
+                            
                             <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" id="collapseEleven" data-bs-parent="#accordion" >
                                 <a className="dropdown-item" onClick={this.logout} href="" data-toggle="modal" data-target="#logoutModal">
                                     <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
