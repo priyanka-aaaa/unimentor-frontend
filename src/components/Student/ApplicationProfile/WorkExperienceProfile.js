4bb52033f982f4ba4726c154c00cb048bd86ae57
Priyanka <priyanka.calinfo500@gmail.com>
2022-03-06 13:35:58 +0530

    change in work experience

--- src/components/Student/ApplicationProfile/WorkExperienceProfile.js
+++ src/components/Student/ApplicationProfile/WorkExperienceProfile.js
@@ -1,7 +1,10 @@
 
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faTrash
+} from '@fortawesome/free-solid-svg-icons';
 const WorkExperienceProfile = () => {
     const [successMessage, setsuccessMessage] = useState("");
     const [submitSuccess, setsubmitSuccess] = useState("0");
@@ -15,7 +18,7 @@ const WorkExperienceProfile = () => {
         if (localStorage.getItem("userData")) {
             var a = localStorage.getItem('userData');
             var mydata = JSON.parse(a);
-         
+
             var studentId = mydata.data.student._id;
             var mounted = mydata.data.token;
         }
@@ -74,13 +77,29 @@ const WorkExperienceProfile = () => {
                             setsuccessMessage("Work Experience Updated")
                             setTimeout(() => setsubmitSuccess(""), 3000);
                             setsubmitSuccess(1)
+                            const url = process.env.REACT_APP_SERVER_URL + 'student/experiences';
+                            fetch(url, {
+                                method: 'GET',
+                                headers: {
+                                    'Authorization': mounted,
+
+                                }
+                            })
+                                .then(response => response.json())
+                                .then(data => {
+                                    var myresults = data.studentExperiences;
+                                    if (Object.keys(myresults).length === 0) {
+
+                                    }
+                                    else {
+                                        setFormValues(data.studentExperiences)
+                                    }
+                                })
                         }
-                        else {
-                  
-                        }
+
                     })
                     .catch(error => {
-                  
+
                     });
 
             }
@@ -94,22 +113,44 @@ const WorkExperienceProfile = () => {
                             setsuccessMessage("Work Experience Updated")
                             setTimeout(() => setsubmitSuccess(""), 3000);
                             setsubmitSuccess(1)
+                            const url = process.env.REACT_APP_SERVER_URL + 'student/experiences';
+                            fetch(url, {
+                                method: 'GET',
+                                headers: {
+                                    'Authorization': mounted,
+
+                                }
+                            })
+                                .then(response => response.json())
+                                .then(data => {
+                                    var myresults = data.studentExperiences;
+                                    if (Object.keys(myresults).length === 0) {
+
+                                    }
+                                    else {
+                                        setFormValues(data.studentExperiences)
+                                    }
+                                })
                         }
-                        else {
-                          
-                        }
+
                     })
                     .catch(error => {
-                 
+
                     });
             }
         })
 
 
     }
+
+    function setworkStatus(i, myvalue) {
+        let newFormValues = [...formValues];
+        newFormValues[i]["status"] = myvalue;
+        setFormValues(newFormValues);
+    }
     function handleDelete(value) {
         const url2 = process.env.REACT_APP_SERVER_URL + 'student/experiences/' + value
-  
+
         fetch(url2, {
             method: 'delete',
             headers: { 'Authorization': mounted }
@@ -121,7 +162,7 @@ const WorkExperienceProfile = () => {
                 setsuccessMessage("Family Deleted")
                 setTimeout(() => setsubmitSuccess(""), 3000);
                 setsubmitSuccess(1)
-               
+
                 const url = process.env.REACT_APP_SERVER_URL + 'student/experiences';
                 fetch(url, {
                     method: 'GET',
@@ -132,18 +173,18 @@ const WorkExperienceProfile = () => {
                 })
                     .then(response => response.json())
                     .then(data => {
-                        var myresults = data.studentExperience;
+                        var myresults = data.studentExperiences;
                         if (Object.keys(myresults).length === 0) {
 
                         }
                         else {
-                            setFormValues(data.studentExperience)
+                            setFormValues(data.studentExperiences)
                         }
                     })
-              
+
             })
     }
- 
+
     return (
         <div>
             {submitSuccess === 1 ? <div className="Show_success_message">
@@ -159,7 +200,11 @@ const WorkExperienceProfile = () => {
 
                         {formValues.map((element, index) => (
                             <div key={index}>
-                                <a onClick={() => handleDelete(element._id)}><i className="fas fa-trash-alt"></i></a>
+                                <a onClick={() => handleDelete(element._id)}>
+                                    <FontAwesomeIcon icon={faTrash} />
+
+
+                                </a>
 
                                 <div className="row">
                                     <div className="col-12 col-sm-4 col-md-4 col-lg-4">
@@ -168,24 +213,34 @@ const WorkExperienceProfile = () => {
                                                 *</span></label><br />
                                             <label className="ant-radio-wrapper"><span className="ant-radio">
                                                 <input
-                                                    value={element.status || ""} onChange={e => handleChange(index, e)}
+                                                   required
+                                                    onChange={(e) => setworkStatus(index, "Ongoing")}
+                                                    checked={element.status === "Ongoing"}
+                                                 
 
 
-                                                    name="status" type="radio" className="ant-radio-input" value="Ongoing" />
+
+
+                                                    name="status" type="radio" className="ant-radio-input" />
                                                 <span className="ant-radio-inner"></span></span><span>Ongoing</span></label>
                                             <label className="ant-radio-wrapper ant-radio-wrapper-checked"><span className="ant-radio ant-radio-checked"><input
-                                                value={element.status || ""} onChange={e => handleChange(index, e)}
+                                               required
+                                                onChange={(e) => setworkStatus(index, "Completed")}
+                                                checked={element.status === "Completed"}
+                                             
 
-                                                name="status" type="radio" className="ant-radio-input" value="Completed" /><span className="ant-radio-inner"></span></span><span>Completed</span></label>
+
+
+                                                name="status" type="radio" className="ant-radio-input" /><span className="ant-radio-inner"></span></span><span>Completed</span></label>
                                             <br />
                                         </div>
                                     </div>
                                     <div className="col-12 col-sm-4 col-md-4 col-lg-4">
                                         <div className="form-group"><label htmlFor="work_type">Work Type <span className="text-danger"> *</span></label><select
                                             value={element.type || ""} onChange={e => handleChange(index, e)}
-
+                                            required
                                             className="form-control" id="work_type" name="type">
-                                            <option >Select</option>
+                                            <option value="">Select</option>
                                             <option value="Full-time">Full-time</option>
                                             <option value="Part-time">Part-time</option>
                                             <option value="Internship">Internship</option>
@@ -196,7 +251,7 @@ const WorkExperienceProfile = () => {
                                             Organization <span className="text-danger">
                                                 *</span></label><input
                                                 value={element.organization || ""} onChange={e => handleChange(index, e)}
-
+required
 
                                                 type="text" className="form-control" id="company_name" name="organization" placeholder="Name of Organization" /></div>
                                     </div>
@@ -239,7 +294,7 @@ const WorkExperienceProfile = () => {
                                             <input
                                                 value={element.ended || ""} onChange={e => handleChange(index, e)}
 
-                                                type="date" className="form-control" name="ended"/>
+                                                type="date" className="form-control" name="ended" />
                                         </div>
                                     </div>
                                 </div>
@@ -267,7 +322,7 @@ const WorkExperienceProfile = () => {
 
                                                 type="text"
                                                 className="form-control" id="job_city"
-                                                placeholder="City/Town" name="city"  /></div>
+                                                placeholder="City/Town" name="city" /></div>
                                     </div>
 
                                 </div>
@@ -278,7 +333,7 @@ const WorkExperienceProfile = () => {
                                 <div className="col-md-6"></div>
                                 <div className="col-md-6 text-right">
 
-                                    <button className="button add" type="button" className="btn btn-success " onClick={() => addFormFields()}>Add New</button>
+                                    <button type="button" className="btn btn-success " onClick={() => addFormFields()}>Add New</button>
 
                                     <button type="submit" className="btn btn-secondary">Save
                                     </button>
