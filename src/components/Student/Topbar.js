9db9027ec3b306e027986888c43251e0fe22556b
Priyanka <priyanka.calinfo500@gmail.com>
2022-03-10 10:03:03 +0530

    change in topbar

--- src/components/Student/Topbar.js
+++ src/components/Student/Topbar.js
@@ -39,7 +39,10 @@ class Topbar extends Component {
             <div>
                 <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
                     <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
-                        <i className="fa fa-bars"></i>
+                        
+                     
+                        <FontAwesomeIcon icon={faBars} />
+
                     </button>
                     <form
                         className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
