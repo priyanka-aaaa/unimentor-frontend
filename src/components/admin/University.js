4a0bbdbb037d163c36169c412a8bd3886729b0d9
Priyanka <priyanka.calinfo500@gmail.com>
2022-03-16 14:53:04 +0530

    change in university

--- src/components/admin/University.js
+++ src/components/admin/University.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import SweetAlert from 'react-bootstrap-sweetalert';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faTrash, faPen, faEye, faUserSlash
 
 } from '@fortawesome/free-solid-svg-icons';
+import Loader from '../Home/Loader';
 export default function AdminUniversity() {
     const [mounted, setMounted] = useState();
     const [data, setdata] = useState([]);
@@ -13,7 +15,11 @@ export default function AdminUniversity() {
     const [formValues, setFormValues] = useState([{
         name: "", email: "", phone: "", _id: ""
     }])
-
+    const [successMessage, setsuccessMessage] = useState("");
+    const [submitSuccess, setsubmitSuccess] = useState("0");
+    const [loader, setmyloader] = useState("false");
+    const [showSweetAlert, setshowSweetAlert] = useState("0");
+    const [deleteId, setdeleteId] = useState("");
     useEffect(() => {
         var mounted = localStorage.getItem("adminToken")
         setMounted(mounted)
@@ -28,11 +34,60 @@ export default function AdminUniversity() {
                 setFormValues(data.universities)
             })
     }, [])
+    let handleDeleteClick = (value) => {
+        setshowSweetAlert("1")
+        setdeleteId(value)
+    }
     return (
         <div className="container">
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">University</h1>
             </div>
+            {submitSuccess === 1 ? <div className="Show_success_message">
+                <strong>Success!</strong> {successMessage}
+            </div> : null}
+            {loader === "true" ?
+                <Loader />
+                : null}
+            {showSweetAlert === "1" ? <SweetAlert
+                warning
+                showCancel
+                confirmBtnText="Yes, delete it!"
+                confirmBtnBsStyle="danger"
+                title="Are you sure?"
+                onConfirm={(value) => {
+                    setshowSweetAlert("0");
+                    setmyloader("true")
+                    axios.delete(process.env.REACT_APP_SERVER_URL + 'admin/deleteUniversity/' + deleteId, { headers: { 'Authorization': mounted } })
+                        .then(function (res) {
+                            setmyloader("false")
+                            if (res.data.success === true) {
+                                setsuccessMessage("University deleted")
+                                setTimeout(() => setsubmitSuccess(""), 3000);
+                                setsubmitSuccess(1)
+                                const url = process.env.REACT_APP_SERVER_URL + "universities";
+                                fetch(url, {
+                                    method: 'GET',
+                                    headers: { 'Authorization': mounted }
+                                })
+                                    .then(response => response.json())
+                                    .then(data => {
+                                        setdata(data.universities);
+                                        setFormValues(data.universities)
+                                    })
+                            }
+                        })
+                        .catch(error => {
+                        });
+                }}
+                onCancel={() =>
+                    setshowSweetAlert("0")
+                }
+                focusCancelBtn
+            >
+            </SweetAlert>
+                : null
+            }
             <div className="row">
                 <div className="col-xl-12 col-lg-7">
                     <div className="card shadow mb-4">
@@ -66,7 +121,7 @@ export default function AdminUniversity() {
 
 
                                                     <td>
-                                                        <button className="btn btn-danger btn-sm" >
+                                                        <button className="btn btn-danger btn-sm" onClick={() => handleDeleteClick(object._id)}>
                                                             <FontAwesomeIcon icon={faTrash} />
                                                         </button>
                                                         <button className="btn btn-primary btn-sm vbtn" ><Link to={'/institute/' + object.slug} className="nav-link" >
